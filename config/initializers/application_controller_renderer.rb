6cecf21e4bef6fff82fd3e9103ed75c468140e57
kanaharuna <paris.makaron.81@gmail.com>
2021-04-14 00:06:37 +0000

    [Add]画像投稿機能追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '115bdbc2d0dd52632e83e237b0f279faa476ab85aeca86721134bc4d2fe9a56a837b731a6618648cdc3a3ac7bbe7e97104b130ddfcaed67aa732afeccc716347'
